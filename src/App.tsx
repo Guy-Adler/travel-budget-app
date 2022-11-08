ff3a28a15b9e8cb1131497aee5ba9aefcb6ef97e
Guy Adler <guyadler2005@gmail.com>
2022-11-08 18:53:57 +0200

    added data provider
    wished for it to be more ts friendly, but can't seem to get it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Admin, Resource, CustomRoutes } from 'react-admin';
+import { Admin, Resource, CustomRoutes, ListGuesser } from 'react-admin';
 import { supabaseAuthProvider } from './providers/auth';
+import { supabaseDataProvider } from './providers/data';
 import i18nProvider from './providers/i18n';
 import LogIn from './views/auth/LogIn';
 import Layout from './layout';
 import Profile from './views/profile';
+
+const resources = {
+  profiles: ['id', 'email', 'first_name', 'last_name', 'avatar_url'],
+  trips: ['id', 'created_at', 'updated_at', 'updated_by', 'owner', 'name'],
+  shares: ['id', 'trip_id', 'user_id', 'can_edit'],
+  expenses: ['id', 'trip_id', 'created_at', 'updated_at', 'updated_by', 'name', 'amount', 'currency', 'purchase_date', 'category']
+};
 
 const App = () => (
   <Admin
@@ -13,6 +21,7 @@ const App = () => (
     layout={Layout}
     loginPage={LogIn}
     authProvider={supabaseAuthProvider}
+    dataProvider={supabaseDataProvider(resources)}
     requireAuth
     i18nProvider={i18nProvider}
     disableTelemetry
@@ -20,7 +29,10 @@ const App = () => (
     <CustomRoutes>
       <Route path="/profile" element={<Profile />} />
     </CustomRoutes>
-    <Resource name="profiles" />
+    <Resource name="profiles" list={ListGuesser} />
+    <Resource name="trips" list={ListGuesser} />
+    <Resource name="shares" list={ListGuesser} />
+    <Resource name="expenses" list={ListGuesser} />
   </Admin>
 );
 
--- src/providers/data/index.ts
+++ src/providers/data/index.ts
@@ -1,1 +1,216 @@
-// TODO add the provider
+import type { DataProvider, GetListParams } from 'react-admin';
+import client from '../supabase';
+
+type ResourceOptionsWithFullTextSearch = {
+  table?: string;
+  primaryKey?: string;
+  fields: string[];
+  fullTextSearchFields?: string[];
+};
+type InternalResourceOptions = Required<ResourceOptionsWithFullTextSearch>;
+export type ResourceOptions = string[] | ResourceOptionsWithFullTextSearch;
+export type ResourcesOptions = Record<string, ResourceOptions>;
+
+const getResourceOptions = (
+  resource: string,
+  options: ResourcesOptions
+): InternalResourceOptions => {
+  const resourceOptions = options[resource];
+
+  if (Array.isArray(resourceOptions)) {
+    return {
+      table: resource,
+      primaryKey: 'id',
+      fields: resourceOptions,
+      fullTextSearchFields: resourceOptions,
+    };
+  }
+
+  return {
+    table: resourceOptions.table ?? resource,
+    primaryKey: resourceOptions.primaryKey ?? 'id',
+    fields: resourceOptions.fields,
+    fullTextSearchFields:
+      resourceOptions.fullTextSearchFields ?? resourceOptions.fields,
+  };
+};
+
+const getList = async ({
+  resourceOptions,
+  params,
+}: {
+  resourceOptions: InternalResourceOptions;
+  params: GetListParams;
+}) => {
+  const {
+    pagination,
+    sort,
+    filter: { q, ...filter },
+  } = params;
+
+  const rangeFrom = (pagination.page - 1) * pagination.perPage;
+  const rangeTo = rangeFrom + pagination.perPage;
+
+  let query = client
+    .from(resourceOptions.table)
+    .select(resourceOptions.fields.join(', '), { count: 'exact' })
+    .order(sort.field, { ascending: sort.order === 'ASC' })
+    .match(filter)
+    .range(rangeFrom, rangeTo);
+
+  if (q) {
+    const fullTextSearchFields = Array.isArray(resourceOptions)
+      ? resourceOptions
+      : resourceOptions.fullTextSearchFields;
+
+    fullTextSearchFields.forEach((field) => {
+      query = query.ilike(field, `%${q}%`);
+    });
+  }
+
+  const { data, error, count } = await query;
+
+  if (error) {
+    throw new Error(`(${error.code}) ${error.message}`);
+  }
+  return {
+    data:
+      resourceOptions.primaryKey === 'id'
+        ? data
+        : data.map((item) => ({
+            ...item,
+            id: item[resourceOptions.primaryKey],
+          })) ?? [],
+    total: count ?? 0,
+  };
+};
+
+export const supabaseDataProvider = (
+  resources: ResourcesOptions
+): DataProvider => ({
+  async getList(resource, params) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    return getList({ resourceOptions, params });
+  },
+  async getOne(resource, { id }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data, error } = await client
+      .from(resourceOptions.table)
+      .select(resourceOptions.fields.join(', '))
+      .match({ id })
+      .single();
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+
+    if (resourceOptions.primaryKey === 'id') {
+      return { data };
+    }
+
+    return { ...data, id: data[resourceOptions.primaryKey] };
+  },
+  async getMany(resource, { ids }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+
+    const { data, error } = await client
+      .from(resourceOptions.table)
+      .select(resourceOptions.fields.join(', '))
+      .in('id', ids);
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+    return { data: data ?? [] };
+  },
+  async getManyReference(resource, originalParams) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { target, id } = originalParams;
+    const params = {
+      ...originalParams,
+      filter: { ...originalParams.filter, [target]: id },
+    };
+    return getList({ resourceOptions, params });
+  },
+  async create(resource, { data }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data: record, error } = await client
+      .from(resourceOptions.table)
+      .insert(data)
+      .single();
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+
+    if (resourceOptions.primaryKey === 'id') {
+      return { data: record };
+    }
+
+    return { ...record, id: record[resourceOptions.primaryKey] };
+  },
+  async update(resource, { id, data }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data: record, error } = await client
+      .from(resourceOptions.table)
+      .update(data)
+      .match({ id })
+      .single();
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+
+    if (resourceOptions.primaryKey === 'id') {
+      return { data: record };
+    }
+
+    return { ...record, id: record[resourceOptions.primaryKey] };
+  },
+  async updateMany(resource, { ids, data }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data: records, error } = await client
+      .from(resourceOptions.table)
+      .update(data)
+      .in('id', ids);
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+    return {
+      data: records?.map((record) => record[resourceOptions.primaryKey]),
+    };
+  },
+  async delete(resource, { id }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data: record, error } = await client
+      .from(resourceOptions.table)
+      .delete()
+      .match({ id })
+      .single();
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+
+    if (resourceOptions.primaryKey === 'id') {
+      return { data: record };
+    }
+
+    return { ...record, id: record[resourceOptions.primaryKey] };
+  },
+  async deleteMany(resource, { ids }) {
+    const resourceOptions = getResourceOptions(resource, resources);
+    const { data: records, error } = await client
+      .from(resourceOptions.table)
+      .delete()
+      .in('id', ids);
+
+    if (error) {
+      throw new Error(`(${error.code}) ${error.message}`);
+    }
+    return {
+      data: records?.map((record) => record[resourceOptions.primaryKey]),
+    };
+  },
+});
