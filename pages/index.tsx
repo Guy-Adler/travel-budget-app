f6b248fdc91faf66aaa093e9e67340f6c7baebd1
Guy Adler <guyadler2005@gmail.com>
2022-11-25 12:32:11 +0200

    fixed avatar urls of google sometimes returning 403

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const App = dynamic(() => import('../src/App'), { ssr: false });
 
-const Home: NextPage = () => <App />;
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <meta name="referrer" content="no-referrer" /> {/* https://github.com/chakra-ui/chakra-ui/issues/5909 */}
+    </Head>
+    <App />
+  </>
+);
 
 export default Home;
