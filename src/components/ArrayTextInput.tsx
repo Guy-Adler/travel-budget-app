3619e737f3f6b259e5dee87ad033a2867da09ddb
Guy Adler <guyadler2005@gmail.com>
2022-11-25 23:27:48 +0200

    handled focusing and moving around tabs

--- src/components/ArrayTextInput.tsx
+++ src/components/ArrayTextInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import Chip from '@mui/material/Chip';
 import { AvatarProps } from '@mui/material/Avatar';
@@ -62,6 +62,56 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
   }) as ArrayTextUseInputValue;
 
   const [chipsError, setChipsErrors] = useState<(string | boolean)[]>([]);
+  const [focusedTag, setFocusedTag] = useState(-1);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const anchorRef = useRef<HTMLDivElement | null>(null);
+
+  const moveFocus = (tagIndex: number) => {
+    if (tagIndex === -1) {
+      inputRef.current?.focus();
+    } else {
+      const tag = anchorRef.current?.querySelector(`[data-tag-index="${tagIndex}"]`) as HTMLElement | null | undefined;
+      tag?.focus();
+    }
+  };
+
+  const handleFocusTag = (direction: 'previous' | 'next') => {
+    let nextTag = focusedTag;
+    if (focusedTag === -1) {
+      if (inputRef.current?.value === '' && direction === 'previous') {
+        nextTag = value.length - 1;
+      }
+    } else {
+      nextTag += direction === 'next' ? 1 : -1;
+
+      if (nextTag < 0) {
+        nextTag = 0;
+      }
+
+      if (nextTag === value.length) {
+        nextTag = -1;
+      }
+    }
+
+    setFocusedTag(nextTag);
+  };
+
+  const deleteTag = (idx: number) => {
+    setValue([...value.slice(0, idx), ...value.slice(idx + 1)]);
+    setChipsErrors((errors) => [
+      ...errors.slice(0, idx),
+      ...errors.slice(idx + 1),
+    ]);
+    setFocusedTag(-1);
+  };
+
+  useEffect(() => {
+    if (focusedTag < -1 || !Number.isInteger(focusedTag) || focusedTag > chipsError.length) {
+      setFocusedTag(-1);
+    } else {
+      moveFocus(focusedTag);
+    }
+  }, [focusedTag, chipsError.length]);
 
   return (
     <TextField
@@ -74,6 +124,18 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
         fieldOnBlur();
       }}
       onKeyDown={async (e) => {
+        // Handle tag focus change
+        if (focusedTag !== -1 && !['ArrowLeft', 'ArrowRight'].includes(e.key)) {
+          setFocusedTag(-1);
+        }
+        if (e.key === 'ArrowLeft') {
+          handleFocusTag('previous');
+        }
+        if (e.key === 'ArrowRight') {
+          handleFocusTag('next');
+        }
+    
+        // Handle Tag creation
         if ([...newTagKeys, 'Enter'].includes(e.key)) {
           e.preventDefault();
           e.stopPropagation();
@@ -86,12 +148,13 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
             setChipsErrors([...chipsError, await valuesValidator(newValue)]);
           }
         }
+
+        // Handle tag deletion
         if (
           e.key === 'Backspace' &&
           (e.target as HTMLInputElement).value === ''
         ) {
-          setValue(value.slice(0, -1));
-          setChipsErrors((errors) => errors.slice(0, -1));
+          deleteTag(value.length - 1);
         }
       }}
       ref={ref}
@@ -105,6 +168,7 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
         maxWidth: '100%',
       }}
       InputProps={{
+        ref: anchorRef,
         startAdornment: value.map((val, idx) => (
           <Chip
             key={Math.random()}
@@ -113,6 +177,7 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
               idx < chipsError.length && chipsError[idx] !== false ? 'error' : undefined
             }
             variant="outlined"
+            data-tag-index={idx}
             icon={
               /* because otherwise there is a style issue */ // eslint-disable-next-line no-nested-ternary
               idx >= chipsError.length ? (
@@ -121,19 +186,18 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
                 <ErrorIcon />
               ) : undefined
             }
-            avatar={Avatar && (idx < chipsError.length || chipsError[idx] === false) ? <Avatar value={val} /> : undefined}
+            avatar={Avatar && idx < chipsError.length && chipsError[idx] === false ? <Avatar value={val} /> : undefined}
             size="small"
-            onDelete={() => {
-              setValue([...value.slice(0, idx), ...value.slice(idx + 1)]);
-              setChipsErrors((errors) => [
-                ...errors.slice(0, idx),
-                ...errors.slice(idx + 1),
-              ]);
-            }}
+            onDelete={() => deleteTag(idx)}
             sx={{
               mr: 1,
               mt: 1,
               mb: 'auto',
+            }}
+            onKeyDown={(e) => {
+              if (['Backspace', 'Delete'].includes(e.key)) {
+                deleteTag(idx);
+              }
             }}
           />
         )),
@@ -143,6 +207,7 @@ const ArrayTextInput: React.FC<ArrayTextInputProps> = ({
         },
       }}
       inputProps={{
+        ref: inputRef,
         style: {
           display: 'block',
           flex: '1 1',
