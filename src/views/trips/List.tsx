f11e221b6e4a80cf76cdd7db3205d902f3229c2e
Guy Adler <guyadler2005@gmail.com>
2022-12-07 18:05:57 +0200

    added filtering to the trips

--- src/views/trips/List.tsx
+++ src/views/trips/List.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { List, TopToolbar, ExportButton, SortButton } from 'react-admin';
+import {
+  List,
+  TopToolbar,
+  ExportButton,
+  SortButton,
+  FilterButton,
+  NullableBooleanInput,
+  TextInput,
+} from 'react-admin';
 import TripsIterator from './TripsIterator';
 import Empty from './Empty';
 import CreateTrip from './CreateTrip';
@@ -8,11 +16,30 @@ import CreateButton from './CreateButton';
 
 const TripsActions = () => (
   <TopToolbar>
+    <FilterButton disableSaveQuery />
     <SortButton fields={['trip_name', 'updated_at']} />
     <CreateButton />
     <ExportButton />
   </TopToolbar>
 );
+
+const tripFilters = [
+  <TextInput
+    key={0}
+    label="Search"
+    source="q"
+    alwaysOn
+    sx={{ ml: 1.5 }} // To offset the margin reset of the grid (-12px on desktop)
+  />,
+  <NullableBooleanInput
+    key={1}
+    source="is_owner"
+    label="Ownership"
+    nullLabel="Both"
+    falseLabel="Shared"
+    trueLabel="Owned"
+  />,
+];
 
 const TripList = () => {
   const dialogState = useState(false);
@@ -20,6 +47,7 @@ const TripList = () => {
   return (
     <DialogContext.Provider value={dialogState}>
       <List
+        filters={tripFilters}
         empty={<Empty />}
         emptyWhileLoading
         sort={{ field: 'updated_at', order: 'DESC' }}
